840298efe773f59a22b38dd6711ea9aee134571d
Jamie Pinheiro <jamiepinheiro@gmail.com>
2021-05-12 13:42:07 -0400

    Switch to WS Trade API was trade accounts

--- main.py
+++ main.py
@@ -9,59 +9,86 @@ from datetime import datetime, timedelta
 from config import *
 
 WS_OAUTH_URL = "https://api.production.wealthsimple.com/v1/oauth/token"
-WS_TRADE_BALANCE_URL = "https://trade-service.wealthsimple.com/account/history/1m?account_id="
+WS_TRADE_AUTH_URL = "https://trade-service.wealthsimple.com/auth/login"
+WS_TRADE_BALANCE_URL = "https://trade-service.wealthsimple.com/account/history/1d?account_id="
 WS_NON_TRADE_BALANCE_URL = "https://my.wealthsimple.com/graphql"
 BALANCE_GRAPHQL_QUERY = "{\"operationName\":\"getPortfolioValue\",\r\n\"variables\":{\"clientId\":\"%s\",\"currency\":\"CAD\",\"today\":\"%s\",\"tomorrow\":\"%s\"},\"query\":\"query getPortfolioValue($clientId: ID!, $currency: Currency!, $today: String!, $tomorrow: String!) {\\n  client(id: $clientId) {\\n    id\\n    profile {\\n      preferredFirstName: preferred_first_name\\n      __typename\\n    }\\n    ...ClientAccounts\\n    netDeposits: net_deposits(start_date: $today, end_date: $tomorrow, resolution: daily) {\\n      ...ClientEmptyStateNetDeposits\\n      results {\\n        accountId: account_id\\n        totalWithdrawals: total_withdrawals {\\n          amount\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    netLiquidationValues: net_liquidation_values(resolution: daily, start_date: $today, end_date: $tomorrow, currency: $currency) {\\n      ...ClientNetLiquidationValue\\n      date\\n      accountId: account_id\\n      netLiquidationValue: net_liquidation_value\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment ClientAccounts on Client {\\n  id\\n  accounts {\\n    id\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment ClientNetLiquidationValue on NetLiquidationValue {\\n  date\\n  netLiquidationValue: net_liquidation_value\\n  __typename\\n}\\n\\nfragment ClientEmptyStateNetDeposits on NetDepositsResponse {\\n  results {\\n    totalDeposits: total_deposits {\\n      amount\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n\"}"
 LUNCH_MONEY_ASSET_URL = "https://dev.lunchmoney.app/v1/assets/"
 
-class WsSession(NamedTuple):
+class WsNonTradeSession(NamedTuple):
 	accessToken: str
 	refreshToken: str
 	userId: str
+
+class WsTradeSession(NamedTuple):
+	accessToken: str
+	refreshToken: str
+
+class WsSessions(NamedTuple):
+	ws: WsNonTradeSession
+	wsTrade: WsTradeSession
 
 class Balance(NamedTuple):
 	amount: str
 	asOf: str
 
-def loginToWs(optCode: str):
-	result = requests.post(
-		WS_OAUTH_URL,
-		data = {
-			"grant_type": "password",
-			"username": WS_USERNAME,
-			"password": WS_PASSWORD,
-			"skip_provision": True,
-			"otp_claim": None,
-			"scope": "invest.read invest.write mfda.read mfda.write mercer.read mercer.write trade.read trade.write empower.read empower.write tax.read tax.write",
-			"client_id": "4da53ac2b03225bed1550eba8e4611e086c7b905a3855e6ed12ea08c246758fa"
-		},
-		headers = {
-			"x-wealthsimple-otp": optCode
-		}
-	)
-	response = json.loads(result.content)
-	return WsSession(response["access_token"], response["refresh_token"], response["resource_owner_id"])
+def loginToWs(nonWsTradeAccounts: bool, wsTradeAccounts: bool, optCode: str):
+	ws, wsTrade = None, None
 
-def getWsTradeBalance(ws: WsSession, assetLink: AssetLink):
+	if nonWsTradeAccounts:
+		result = requests.post(
+			WS_OAUTH_URL,
+			data = {
+				"grant_type": "password",
+				"username": WS_USERNAME,
+				"password": WS_PASSWORD,
+				"skip_provision": True,
+				"otp_claim": None,
+				"scope": "invest.read invest.write mfda.read mfda.write mercer.read mercer.write trade.read trade.write empower.read empower.write tax.read tax.write",
+				"client_id": "4da53ac2b03225bed1550eba8e4611e086c7b905a3855e6ed12ea08c246758fa"
+			},
+			headers = {
+				"x-wealthsimple-otp": optCode
+			}
+		)
+		response = json.loads(result.content)
+		ws = WsNonTradeSession(response["access_token"], response["refresh_token"], response["resource_owner_id"])
+		logging.info("Logged into Ws Non Trade")
+
+	if wsTradeAccounts:
+		result = requests.post(
+			WS_TRADE_AUTH_URL,
+			data = {
+				"email": WS_USERNAME,
+				"password": WS_PASSWORD,
+				"otp": optCode
+			}
+		)
+		wsTrade = WsTradeSession(result.headers["X-Access-Token"], result.headers["X-Refresh-Token"])
+		logging.info("Logged into Ws Trade")
+	
+	return WsSessions(ws, wsTrade)
+
+def getWsTradeBalance(wsSessions: WsSessions, assetLink: AssetLink):
 	result = requests.get(
 		WS_TRADE_BALANCE_URL + assetLink.wsAccountId,
 		headers = {
-			"Authorization": "Bearer " + ws.accessToken
+			"Authorization": "Bearer " + wsSessions.ws.accessToken
 		}
 	)
 	balance = json.loads(result.content)["results"][-1]
 	
 	return Balance(balance["value"]["amount"], balance["date"])
 
-def getWsNonTradeBalance(ws: WsSession, assetLink: AssetLink):
+def getWsNonTradeBalance(wsSessions: WsSessions, assetLink: AssetLink):
 	response = requests.post(
 		WS_NON_TRADE_BALANCE_URL,
 		headers={
-			"Authorization": "Bearer " + ws.accessToken,
+			"Authorization": "Bearer " + wsSessions.ws.accessToken,
 			"Content-Type": "application/json"
 		}, 
 		data=BALANCE_GRAPHQL_QUERY % (
-			ws.userId,
+			wsSessions.ws.userId,
 			datetime.today().strftime("%Y-%m-%d"),
 			(datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
 		)
@@ -106,8 +133,12 @@ if len(sys.argv) != 2:
 otpCode = sys.argv[1]
 
 logging.info("Logging in and syncing %s's Wealthsimple Accounts" % WS_USERNAME)
+
+nonWsTradeAccounts = any(map(lambda assetLink: not assetLink.isWsTradeAccount, ASSET_LINKS))
+wsTradeAccounts = any(map(lambda assetLink: assetLink.isWsTradeAccount, ASSET_LINKS))
+
 try:
-	ws = loginToWs(otpCode)
+	ws = loginToWs(nonWsTradeAccounts, wsTradeAccounts, otpCode)
 except Exception as e:
 	logging.exception("Failed to login")
 	sys.exit(1)
