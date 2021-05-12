23cec7747c0baa65b262ac95478e88e5e5ff8ad4
Jamie Pinheiro <jamiepinheiro@gmail.com>
2021-05-12 17:34:25 -0400

    Update sample scripts

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@ set LUNCH_MONEY_API_KEY=1234567890abcdefg
 set WS_USERNAME=me@emil.com
 set WS_PASSWORD=mysecurepassword
 set ASSET_LINKS=ws-trade-account-id,True,lunch-money-related-asset-id ws-invest-account-id,False,lunch-money-related-asset-id ws-cash-account-id,False,lunch-money-related-asset-id
-python main.py %1
+python3 main.py %1
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LUNCH_MONEY_API_KEY="1234567890abcdefg"
-WS_USERNAME="me@emil.com"
-WS_PASSWORD="mysecurepassword"
-ASSET_LINKS="ws-trade-account-id,True,lunch-money-related-asset-id ws-invest-account-id,False,lunch-money-related-asset-id ws-cash-account-id,False,lunch-money-related-asset-id"
+export LUNCH_MONEY_API_KEY="1234567890abcdefg"
+export WS_USERNAME="me@emil.com"
+export WS_PASSWORD="mysecurepassword"
+export ASSET_LINKS="ws-trade-account-id,True,lunch-money-related-asset-id ws-invest-account-id,False,lunch-money-related-asset-id ws-cash-account-id,False,lunch-money-related-asset-id"
 python main.py $1
