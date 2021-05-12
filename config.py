86b2d5fd43ad7d6b71363f0446b35184d30ba086
Jamie Pinheiro <jamiepinheiro@gmail.com>
2021-05-12 14:42:55 -0400

    Refresh access tokens

--- config.py
+++ config.py
@@ -19,3 +19,10 @@ def toAssetLink(rawAssetLink: str):
     return AssetLink(parts[0], parts[1] == "True", parts[2])
 
 ASSET_LINKS = list(map(toAssetLink, environ["ASSET_LINKS"].split(" ")))
+
+# How often to refresh access tokens and sync data
+#  (25 minutes worked well in testing)
+REFRESH_INTERVAL_SECS = environ.get("REFRESH_INTERVAL_SECS", 25 * 60)
+
+# How many times to retry logging in before giving up
+RETRY_LOGIN_TIMES = environ.get("RETRY_LOGIN_TIMES", 5)
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from config import *
 
 WS_OAUTH_URL = "https://api.production.wealthsimple.com/v1/oauth/token"
 WS_TRADE_AUTH_URL = "https://trade-service.wealthsimple.com/auth/login"
+WS_TRADE_AUTH_REFRESH_URL = "https://trade-service.wealthsimple.com/auth/refresh"
 WS_TRADE_BALANCE_URL = "https://trade-service.wealthsimple.com/account/history/1d?account_id="
 WS_NON_TRADE_BALANCE_URL = "https://my.wealthsimple.com/graphql"
 BALANCE_GRAPHQL_QUERY = "{\"operationName\":\"getPortfolioValue\",\r\n\"variables\":{\"clientId\":\"%s\",\"currency\":\"CAD\",\"today\":\"%s\",\"tomorrow\":\"%s\"},\"query\":\"query getPortfolioValue($clientId: ID!, $currency: Currency!, $today: String!, $tomorrow: String!) {\\n  client(id: $clientId) {\\n    id\\n    profile {\\n      preferredFirstName: preferred_first_name\\n      __typename\\n    }\\n    ...ClientAccounts\\n    netDeposits: net_deposits(start_date: $today, end_date: $tomorrow, resolution: daily) {\\n      ...ClientEmptyStateNetDeposits\\n      results {\\n        accountId: account_id\\n        totalWithdrawals: total_withdrawals {\\n          amount\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    netLiquidationValues: net_liquidation_values(resolution: daily, start_date: $today, end_date: $tomorrow, currency: $currency) {\\n      ...ClientNetLiquidationValue\\n      date\\n      accountId: account_id\\n      netLiquidationValue: net_liquidation_value\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment ClientAccounts on Client {\\n  id\\n  accounts {\\n    id\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment ClientNetLiquidationValue on NetLiquidationValue {\\n  date\\n  netLiquidationValue: net_liquidation_value\\n  __typename\\n}\\n\\nfragment ClientEmptyStateNetDeposits on NetDepositsResponse {\\n  results {\\n    totalDeposits: total_deposits {\\n      amount\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n\"}"
@@ -53,7 +54,7 @@ def loginToWs(nonWsTradeAccounts: bool, wsTradeAccounts: bool, optCode: str):
 		)
 		response = json.loads(result.content)
 		ws = WsNonTradeSession(response["access_token"], response["refresh_token"], response["resource_owner_id"])
-		logging.info("Logged into Ws Non Trade")
+		logging.info("Logged into Wealthsimple Non Trade")
 
 	if wsTradeAccounts:
 		result = requests.post(
@@ -65,7 +66,7 @@ def loginToWs(nonWsTradeAccounts: bool, wsTradeAccounts: bool, optCode: str):
 			}
 		)
 		wsTrade = WsTradeSession(result.headers["X-Access-Token"], result.headers["X-Refresh-Token"])
-		logging.info("Logged into Ws Trade")
+		logging.info("Logged into Wealthsimple Trade")
 	
 	return WsSessions(ws, wsTrade)
 
@@ -73,11 +74,15 @@ def getWsTradeBalance(wsSessions: WsSessions, assetLink: AssetLink):
 	result = requests.get(
 		WS_TRADE_BALANCE_URL + assetLink.wsAccountId,
 		headers = {
-			"Authorization": "Bearer " + wsSessions.ws.accessToken
+			"Authorization": "Bearer " + wsSessions.wsTrade.accessToken
 		}
 	)
+	data = json.loads(result.content)
+
+	if "results" not in data:
+		raise Exception("Failed to get Wealthsimple trade account data", data)
+
 	balance = json.loads(result.content)["results"][-1]
-	
 	return Balance(balance["value"]["amount"], balance["date"])
 
 def getWsNonTradeBalance(wsSessions: WsSessions, assetLink: AssetLink):
@@ -118,6 +123,39 @@ def updateLunchMoneyAsset(balance: Balance, assetLink: AssetLink):
 
 	return response.content.decode("utf-8")
 
+def refreshTokens(wsSessions: WsSessions):
+	ws, wsTrade = None, None
+	
+	if wsSessions.ws:
+		result = requests.post(
+			WS_OAUTH_URL,
+			data = {
+				"grant_type": "refresh_token",
+				"refresh_token": wsSessions.ws.refreshToken,
+				"username": WS_USERNAME,
+				"password": WS_PASSWORD,
+				"skip_provision": True,
+				"otp_claim": None,
+				"scope": "invest.read invest.write mfda.read mfda.write mercer.read mercer.write trade.read trade.write empower.read empower.write tax.read tax.write",
+				"client_id": "4da53ac2b03225bed1550eba8e4611e086c7b905a3855e6ed12ea08c246758fa"
+			}
+		)
+		response = json.loads(result.content)
+		ws = WsNonTradeSession(response["access_token"], response["refresh_token"], response["resource_owner_id"])
+		logging.info("Refreshed Ws Non Trade access token")
+
+	if wsTradeAccounts:
+		result = requests.post(
+			WS_TRADE_AUTH_REFRESH_URL,
+			data = {
+				"refresh_token": wsSessions.wsTrade.refreshToken
+			}
+		)
+		wsTrade = WsTradeSession(result.headers["X-Access-Token"], result.headers["X-Refresh-Token"])
+		logging.info("Refreshed Ws Trade access token")
+
+	return WsSessions(ws, wsTrade)
+
 if not os.path.isdir("logs"):
 	os.mkdir("logs")
 logging.basicConfig(
@@ -137,20 +175,25 @@ logging.info("Logging in and syncing %s's Wealthsimple Accounts" % WS_USERNAME)
 nonWsTradeAccounts = any(map(lambda assetLink: not assetLink.isWsTradeAccount, ASSET_LINKS))
 wsTradeAccounts = any(map(lambda assetLink: assetLink.isWsTradeAccount, ASSET_LINKS))
 
-try:
-	ws = loginToWs(nonWsTradeAccounts, wsTradeAccounts, otpCode)
-except Exception as e:
-	logging.exception("Failed to login")
+ws = None
+for _ in range(RETRY_LOGIN_TIMES):
+	try:
+		ws = loginToWs(nonWsTradeAccounts, wsTradeAccounts, otpCode)
+	except Exception as e:
+		continue
+
+if not ws:
+	logging.error("Failed to login")
 	sys.exit(1)
 
 while True:
 	for assetLink in ASSET_LINKS:
-		logging.info("Updating asset link - %s" % str(assetLink))
+		logging.info("Updating - %s" % str(assetLink))
 
 		balance = None
 		try:
 			balance = getWsTradeBalance(ws, assetLink) if assetLink.isWsTradeAccount else getWsNonTradeBalance(ws, assetLink)
-			logging.info("Successfully got balance - %s" % str(balance))
+			logging.info("Successfully got - %s" % str(balance))
 		except Exception as e:
 			logging.exception("Failed to get Wealthsimple balance")
 			continue
@@ -161,4 +204,10 @@ while True:
 		except Exception as e:
 			logging.exception("Failed to update Lunch Money asset")
 
-	time.sleep(60)
+	time.sleep(REFRESH_INTERVAL_SECS)
+
+	try:
+		ws = refreshTokens(ws)
+		logging.info("Successfully refreshed access tokens")
+	except Exception as e:
+		logging.exception("Failed to refresh access tokens")
